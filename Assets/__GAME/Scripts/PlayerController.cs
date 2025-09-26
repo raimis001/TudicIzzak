c3a5399070be7e309cf54a662993d624423e7e96
Raimonds Udris <raimonds.udris@exonicus.eu>
2025-09-26 16:08:22 +0300

    Second lesson

--- Assets/__GAME/Scripts/PlayerController.cs
+++ Assets/__GAME/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     public InputAction controlls;
 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 15f;
+
     void OnEnable()
     {
         controlls.Enable();
@@ -25,11 +29,37 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        move = controlls.ReadValue<Vector2>();
+        move = controlls.ReadValue<Vector2>().normalized;
+
+        Vector3 delta = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        delta = transform.position - delta;
+
+        float angle = Mathf.Atan2(delta.y, delta.x) * Mathf.Rad2Deg;        
+        transform.localEulerAngles = new Vector3(0, 0, angle + 90);
+
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            
+            bullet.transform.position = firePoint.position;
+
+            bullet.GetComponent<Rigidbody2D>().linearVelocity = transform.up * bulletSpeed;
+
+            Destroy(bullet, 2f);
+        }
     }
 
     void FixedUpdate()
     {
-        body.MovePosition(body.position + move * Time.deltaTime * speed);
+        Vector2 dir = transform.up;
+
+        body.MovePosition(body.position + dir * move.magnitude * Time.deltaTime * speed);
+
+       
+        
+       
+        
     }
 }
